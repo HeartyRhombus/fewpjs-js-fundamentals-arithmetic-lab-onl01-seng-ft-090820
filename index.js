e50e0b2cba3e24be1b4076ddceee268df40068e6
HeartyRhombus <kaylahmg@gmail.com>
2020-12-17 14:30:11 -0800

    commit tree d69c708e83092f782d9d82b3fb6e53953888b46c

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 10)
+
+let mod = 18%7
+
+let nums = (0, 15, 3 , 7, 16, 48, 11, 5, 4, 3, 20)
+
+const max = Math.max(nums)
